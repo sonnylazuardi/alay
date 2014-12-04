bff11039f3c5cee2d9a27ddcf70b4363a864dc9a
Sonny Lazuardi <sonnylazuardi@gmail.com>
2014-12-04 16:25:51 +0700

    first

---
+++ src/alay/LayoutTree.java
@@ -0,0 +1,10 @@
+package alay;
+
+import java.util.ArrayList;
+
+public class LayoutTree {
+	private ArrayList <LayoutTree> children;
+	private String type;
+	private ArrayList <Attribute> attributes;
+	
+}
