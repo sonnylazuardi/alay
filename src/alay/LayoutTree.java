e1305dd940f9dfbd82b3d50a3fe1d5d081a17469
Ridho Akbarisanto <ridho.akbarisanto@yahoo.com>
2014-12-04 16:43:54 +0700

    Ayo,,

--- src/alay/LayoutTree.java
+++ src/alay/LayoutTree.java
@@ -6,5 +6,30 @@ public class LayoutTree {
 	private ArrayList <LayoutTree> children;
 	private String type;
 	private ArrayList <Attribute> attributes;
+	public ArrayList<LayoutTree> getChildren() {
+		return children;
+	}
+	public void setChildren(ArrayList<LayoutTree> children) {
+		this.children = children;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public LayoutTree(ArrayList<LayoutTree> children, String type,
+			ArrayList<Attribute> attributes) {
+		super();
+		this.children = children;
+		this.type = type;
+		this.attributes = attributes;
+	}
+	public ArrayList<Attribute> getAttributes() {
+		return attributes;
+	}
+	public void setAttributes(ArrayList<Attribute> attributes) {
+		this.attributes = attributes;
+	}
 	
 }
